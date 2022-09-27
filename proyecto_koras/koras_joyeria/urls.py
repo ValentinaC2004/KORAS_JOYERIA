b7a185dfc53b2f9d836f7c819b9b5c04ab3b3f9f
Valen <vcossio2004@gmail.com>
2022-09-27 16:40:32 -0500

    Organizar imagenes de carousel y agregar animaciones en las cards

--- proyecto_koras/koras_joyeria/urls.py
+++ proyecto_koras/koras_joyeria/urls.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 from django.urls import path
 from koras_joyeria import views
 
-app_name = 'koras'
+app_name = 'koras_joyeria'
 urlpatterns = [
     path('', views.Home , name='home'),
-    path('', views.Registro , name='registro'),
-    path('', views.Inicio , name='inicio'),
+    path('registro/', views.Registro , name='registro'),
+    path('inicio/', views.Inicio , name='inicio'),
 ]
 
 
