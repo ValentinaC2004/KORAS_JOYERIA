6dfc905185757c7dff19c59b11b7bc0d0017c4c8
valentinacossio_ <vcossio2004@gmail.com>
2022-10-13 16:35:48 -0500

    Agregando funciones para el carrito de compras

--- proyecto_koras/koras_joyeria/urls.py
+++ proyecto_koras/koras_joyeria/urls.py
@@ -8,6 +8,7 @@ from koras_joyeria import views
 app_name = 'koras_joyeria'
 urlpatterns = [
     path('', views.Tienda , name='tienda'),
+    path('ver-carrito/', views.VerCarrito, name='ver-carrito'),
     path('home/', views.Home , name='home'),
     path('registro/', views.Registro , name='registro'),
     path('inicio/', LoginView.as_view(template_name='koras_joyeria/ingreso/inicio.html'), name='inicio'),
@@ -16,6 +17,11 @@ urlpatterns = [
     path('inquietudes/', views.Inquietudes , name='inquietudes'),
     path('ver-producto/<int:id>', views.VerProducto, name='ver-producto'),
     path('profile/', views.Profile, name='profile'),
+    path('agregar/<int:producto_id>', views.Agregar_producto, name='Add'),
+    path('eliminar/<int:producto_id>', views.eliminar_producto, name='Remove'),
+    path('restar/<int:producto_id>', views.restar_producto, name='Sub'),
+    path('limpiar/', views.limpiar_carrito, name='CLS'),
+
 ]
 
 
--- proyecto_koras/koras_joyeria/views.py
+++ proyecto_koras/koras_joyeria/views.py
@@ -1,27 +1,50 @@
-from audioop import reverse
-from email import message
-from multiprocessing import context
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from koras_joyeria import Carrito
 from .models import Producto, UserRegisterForm
 # Create your views here.
 
-
-
 def VerProducto(request,id):
     q = Producto.objects.get(pk = id)
-    contexto = {'data':q}
-    return render(request, 'koras_joyeria/tienda/ver-producto.html', contexto)
+    context = {'data':q}
+    return render(request, 'koras_joyeria/tienda/ver-producto.html', context)
 
 def Inquietudes(request):
     return render(request, 'koras_joyeria/info/inquietudes.html')
 
 def Tienda(request):
     q = Producto.objects.all()
-    contexto = {'data': q} 
-    return render(request, 'koras_joyeria/tienda/tienda.html', contexto)
-     
+    context = {'data': q} 
+    return render(request, 'koras_joyeria/tienda/tienda.html', context)
+
+def VerCarrito(request):
+    return render(request, 'koras_joyeria/tienda/ver-carrito.html')
+
+#if carrito
+def Agregar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.agregar(producto)
+    return redirect("koras_joyeria:ver-carrito")
+
+def eliminar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.eliminar(producto)
+    return redirect("koras_joyeria:ver-carrito")
+
+def restar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.restar(producto)
+    return redirect("koras_joyeria:ver-carrito")
+
+def limpiar_carrito(request):
+    carrito = Carrito(request)
+    carrito.limpiar()
+    return redirect("koras_joyeria:ver-carrito")
+
+#endif carrito
 
 def Home(request):
     return render(request, 'koras_joyeria/index.html')
@@ -42,5 +65,5 @@ def Registro(request):
         form = UserRegisterForm()
     context = { 'form' : form }
     return render(request, 'koras_joyeria/ingreso/registro.html',context)
-    
+
 
